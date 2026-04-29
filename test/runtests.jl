446cfeb9370e5e4ae212630a06e0b79b41f2b12e
Michael Cassidy <mc.mathematics@gmail.com>
2026-04-29 20:40:25 +1000

    fix frame operator

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,20 @@
 using ECGTools
+using ECGTools:get_transform
 using Test
 
 @testset "ECGTools.jl" begin
+
+    # "+aVL = ( I - III ) ÷2"""
+    @test get_transform("aVL") == (get_transform("I") - get_transform("III"))/2
+    @test -get_transform("aVR") == (get_transform("I") + get_transform("II"))/2
+    @test -get_transform("aVF") == (get_transform("II") + get_transform("III"))/2
+    # -aVR = ( I + II ) ÷2
+    # +aVF = ( II + III) ÷2")
+
+
+
+
+
     @testset "frame operator" begin
         X = rand(9,100)
         ℱi = InverseFrameOperator()
