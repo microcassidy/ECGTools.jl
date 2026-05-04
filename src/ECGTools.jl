f100a926e2a1204f1940c809fc622267337d1b96
Michael Cassidy <mc.mathematics@gmail.com>
2026-05-04 22:04:10 +1000

    clean up the forward and inverse matrix

--- src/ECGTools.jl
+++ src/ECGTools.jl
@@ -1,96 +1,24 @@
 module ECGTools
-import Base:*,\
 using LinearAlgebra
+using LinearMaps
 
 # Write your package code here.
 const TRANSFORM_LEAD_ORDERING = String["I", "II", "III", "aVR", "aVL", "aVF", "V1", "V2", "V3", "V4", "V5", "V6"]
 
 
-
-const frame_operator = Rational[1 -1 0 0 0 0 0 0 0;
-                                0 -1 1 0 0 0 0 0 0;
-                                -1 0 1 0 0 0 0 0 0;
-                                -1//2 1 -1//2 0 0 0 0 0 0;
-                                1 -1//2 -1//2 0 0 0 0 0 0;
-                                1//2 1//2 -1 0 0 0 0 0 0;
-                                0 2//3 -2//3 1 0 0 0 0 0;
-                                0 2//3 -2//3 0 1 0 0 0 0;
-                                0 2//3 -2//3 0 0 1 0 0 0;
-                                0 2//3 -2//3 0 0 0 1 0 0;
-                                0 2//3 -2//3 0 0 0 0 1 0;
-                                0 2//3 -2//3 0 0 0 0 0 1]
+const frame_operator_X = Rational{Int64}[6 -6 0 0 0 0 0 0 0; 0 -6 6 0 0 0 0 0 0; -6 0 6 0 0 0 0 0 0; -3 6 -3 0 0 0 0 0 0; 6 -3 -3 0 0 0 0 0 0; 3 3 -6 0 0 0 0 0 0; 0 4 -4 6 0 0 0 0 0; 0 4 -4 0 6 0 0 0 0; 0 4 -4 0 0 6 0 0 0; 0 4 -4 0 0 0 6 0 0; 0 4 -4 0 0 0 0 6 0; 0 4 -4 0 0 0 0 0 6]
+const frame_operator_lambda = Rational{Int64}(1//6)
+const frame_operator = LinearMap(frame_operator_X) * frame_operator_lambda
+const ECG_LEAD_IDXS = Int64[[1,3];7:12]
 
 
+const Ψ_terminals= Rational{Int64}[I(8) zeros(8)] #terminals
+const null_space = Rational{Int64}[-ones(3);ones(6)] #null space
 
 
+const F = frame_operator_lambda * LinearMap(Rational{Int64}[ECGTools.frame_operator_X[ECG_LEAD_IDXS,:];inv(frame_operator_lambda) * null_space'])
+const Fi = Rational{Int64}(1//3) * LinearMap(Rational{Int64}[5 3 1 1 1 1 1 1 -1; 2 3 1 1 1 1 1 1 -1; 5 6 1 1 1 1 1 1 -1; 2 2 3 0 0 0 0 0 0; 2 2 0 3 0 0 0 0 0; 2 2 0 0 3 0 0 0 0; 2 2 0 0 0 3 0 0 0; 2 2 0 0 0 0 3 0 0; 2 2 0 0 0 0 0 3 0])
 
 
-get_transform(s::String) = findfirst(s .== TRANSFORM_LEAD_ORDERING) |> idx -> frame_operator[idx,:]
-# const FtF_inverse = [171/289 131/289 59/289 -(48/289) -(48/289) -(48/289) -(48/289) -(48/289) -(48/289);
-# 131/289 195/289 79/289 -(232/867) -(232/867) -(232/867) -(232/867) -(232/867) -(232/867);
-# 59/289 79/289 115/289 24/289 24/289 24/289 24/289 24/289 24/289;
-# -(48/289) -(232/867) 24/289 3209/2601 608/2601 608/2601 608/2601 608/2601 608/2601;
-# -(48/289) -(232/867) 24/289 608/2601 3209/2601 608/2601 608/2601 608/2601 608/2601;
-# -(48/289) -(232/867) 24/289 608/2601 608/2601 3209/2601 608/2601 608/2601 608/2601;
-# -(48/289) -(232/867) 24/289 608/2601 608/2601 608/2601 3209/2601 608/2601 608/2601;
-# -(48/289) -(232/867) 24/289 608/2601 608/2601 608/2601 608/2601 3209/2601 608/2601;
-# -(48/289) -(232/867) 24/289 608/2601 608/2601 608/2601 608/2601 608/2601 3209/2601;]
-#
-#
-using LinearMaps
-const λ_left_inv = 1 // 867
-M = LinearMap(Int64[120 -216 -336 225 405 237 0 0 0 0 0 0; -192 -348 -156 507 219 141 0 0 0 0 0 0; -60 108 168 321 231 315 0 0 0 0 0 0; 88 304 216 -124 8 116 867 0 0 0 0 0; 88 304 216 -124 8 116 0 867 0 0 0 0; 88 304 216 -124 8 116 0 0 867 0 0 0; 88 304 216 -124 8 116 0 0 0 867 0 0; 88 304 216 -124 8 116 0 0 0 0 867 0; 88 304 216 -124 8 116 0 0 0 0 0 867])
-
-const left_inverse_exact = LinearMaps.ScaledMap(λ_left_inv, M)
-
-
-
-# const left_inverse2 = λ*M
-
-# struct LeftInverse <: AbstractMatrix
-
-
-const left_inverse = [ 40/289 -(72/289) -(112/289) 75/289 135/289 79/289 0 0 0 0 0 0;
--(64/289) -(116/289) -(52/289) 169/289 73/289 47/289 0 0 0 0 0 0;
--(20/289) 36/289 56/289 107/289 77/289 105/289 0 0 0 0 0 0;
-88/867 304/867 72/289 -(124/867) 8/867 116/867 1 0 0 0 0 0;
-88/867 304/867 72/289 -(124/867) 8/867 116/867 0 1 0 0 0 0;
-88/867 304/867 72/289 -(124/867) 8/867 116/867 0 0 1 0 0 0;
-88/867 304/867 72/289 -(124/867) 8/867 116/867 0 0 0 1 0 0;
-88/867 304/867 72/289 -(124/867) 8/867 116/867 0 0 0 0 1 0;
-88/867 304/867 72/289 -(124/867) 8/867 116/867 0 0 0 0 0 1]
-
-export FrameOperator,InverseFrameOperator
-struct FrameOperator{Float64} <: AbstractMatrix{Float64}
-    X::Matrix{Float64}
-    FrameOperator() = new{Float64}(frame_operator)
-end
-struct InverseFrameOperator{Float64} <: AbstractMatrix{Float64}
-    X::Matrix{Float64}
-    InverseFrameOperator() = new{Float64}(left_inverse)
-end
-Base.size(m::InverseFrameOperator) = size(m.X)
-Base.length(m::InverseFrameOperator) = length(m.X)
-Base.getindex(m::InverseFrameOperator,i::Integer) = m.X[i]
-Base.getindex(m::InverseFrameOperator,I::Vararg{Int,2}) = m.X[I...]
-
-Base.size(m::FrameOperator) = size(m.X)
-Base.length(m::FrameOperator) = length(m.X)
-Base.getindex(m::FrameOperator,i::Integer) = m.X[i]
-Base.getindex(m::FrameOperator,I::Vararg{Int,2}) = m.X[I...]
-
-# struct FrameOperator
-#     F::ForwardFrameOperator
-#     F_l::InverseFrameOperator
-#     FrameOperator() = new(ForwardOperator(),InverseOperator())
-# end
-
-
-
-
-
-
-# export \,*
-# \(op::InverseFrameOperator,X::AbstractMatrix{Float64}) = op * X
-# *(op::FrameOperator,X::AbstractMatrix{Float64}) = op * X
+get_transform(s::String) = findfirst(s .== TRANSFORM_LEAD_ORDERING) |> idx -> frame_operator_X[idx,:]* frame_operator_lambda
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,25 +1,19 @@
 using ECGTools
 using ECGTools:get_transform
 using Test
+using LinearAlgebra
 
 @testset "ECGTools.jl" begin
 
     # "+aVL = ( I - III ) ÷2"""
-    @test get_transform("aVL") == (get_transform("I") - get_transform("III"))/2
-    @test -get_transform("aVR") == (get_transform("I") + get_transform("II"))/2
-    @test -get_transform("aVF") == (get_transform("II") + get_transform("III"))/2
-    # -aVR = ( I + II ) ÷2
-    # +aVF = ( II + III) ÷2")
-
-
-
+    @testset "operator correctness" begin
+        @test get_transform("aVL") == (get_transform("I") - get_transform("III"))/2
+        @test -get_transform("aVR") == (get_transform("I") + get_transform("II"))/2
+        @test -get_transform("aVF") == (get_transform("II") + get_transform("III"))/2
+    end
 
 
     @testset "frame operator" begin
-        X = rand(9,100)
-        ℱi = InverseFrameOperator()
-        ℱ = FrameOperator()
-        @test ℱi * (ℱ * X) ≈ X
-        @test Float64.(collect(ECGTools.left_inverse2)) == ECGTools.left_inverse
+        @test Matrix(ECGTools.F * ECGTools.Fi) |> M -> M == I
     end
 end
